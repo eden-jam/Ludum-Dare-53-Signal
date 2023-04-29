4e5d343adc8af40ef5413be1844efc8714be2190
FloreauLuca <floreauluca@gmail.com>
2023-04-29 17:56:48 +0200

    Character Inertie

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -11,7 +11,9 @@ namespace DENT
 		#region Fields
 		[SerializeField] private InputActionReference _movementInput;
 		[SerializeField] private InputActionReference _scanInput;
-		[SerializeField] private float _speed = 100;
+		[SerializeField] private float _force = 10;
+		[SerializeField] private float _maxVelocity = 10;
+		[SerializeField] private float _decreaseSpeed = 10;
 		private Rigidbody _rigidbody;
 		[SerializeField] private Transform _renderer = null;
 		[SerializeField] private Scanner _scanner = null;
@@ -26,7 +28,17 @@ namespace DENT
 		private void Update()
 		{
 			Vector2 vector = _movementInput.action.ReadValue<Vector2>();
-			_rigidbody.AddForce(new Vector3(vector.x, 0.0f, vector.y) * _speed);
+			if (vector != Vector2.zero)
+			{
+
+				_rigidbody.AddForce(new Vector3(vector.x, 0.0f, vector.y) * _force);
+				_rigidbody.velocity = _rigidbody.velocity.normalized * Mathf.Min(_rigidbody.velocity.magnitude, _maxVelocity);
+			}
+			else
+			{
+				_rigidbody.AddForce(_rigidbody.velocity * _decreaseSpeed * -1.0f);
+			}
+
 			_renderer.LookAt(transform.position + _rigidbody.velocity);
 			if (_scanInput.action.IsPressed())
 			{
